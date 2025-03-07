8ebe4bb424fa5f4a596943ce29af38919503e382
ismail-hasan-tanjer <tanjerinfo@gmail.com>
2025-03-08 00:50:55 +0600

    what is sql

---
+++ SQL-Practice.sql
@@ -0,0 +1,3 @@
+--What is SQL?
+/*SQL (Structured Query Language) is a programming language used to work with Database Management Systems (DBMS). It is used to retrieve, insert, update, and delete data from relational databases.*/
+
