9e8589b172452e3e4bfa61470775dd51607dedaf
ismail-hasan-tanjer <tanjerinfo@gmail.com>
2025-03-08 01:00:47 +0600

    Retrieving Data

--- SQL-Practice.sql
+++ SQL-Practice.sql
@@ -1,3 +1,11 @@
 --What is SQL?
 /*SQL (Structured Query Language) is a programming language used to work with Database Management Systems (DBMS). It is used to retrieve, insert, update, and delete data from relational databases.*/
 
+--Main functions of SQL:
+
+--Retrieving Data:
+
+--Data can be retrieved from the database using the SELECT statement.
+
+SELECT * FROM Students; 
+
